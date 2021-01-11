0eda5a4bafea01f0132226489dd3a68d0c246c38
asaf0 <hillelassafd@gmail.com>
2021-01-11 18:50:03 +0200

    Sign out button

--- app/src/main/java/com/example/secondtravelapp/UI/NVDActivity.java
+++ app/src/main/java/com/example/secondtravelapp/UI/NVDActivity.java
@@ -8,12 +8,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.secondtravelapp.Models.HistoryDataSource;
 import com.example.secondtravelapp.Models.Travel;
 import com.example.secondtravelapp.Models.UserLocation;
 import com.example.secondtravelapp.R;
+import com.example.secondtravelapp.UI.CompanyTravels.CompanyTravelsFragment;
+import com.example.secondtravelapp.UI.HistoryTravels.HistoryTravelsFragment;
+import com.example.secondtravelapp.UI.RegisteredTravels.RegisteredTravelsFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -33,7 +40,7 @@ public class NVDActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        viewModel = new  ViewModelProvider(this).get(MainTravelsViewModel.class);
+        viewModel = new ViewModelProvider(this).get(MainTravelsViewModel.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nvd);
 
@@ -52,9 +59,29 @@ public class NVDActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-
-
-
+        /*
+        navigationView.setNavigationItemSelectedListener((menuItem) -> {
+            switch (menuItem.getItemId()) {
+                case R.id.nav_sign_out:
+                    Intent intent = new Intent(NVDActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_home:
+                    Intent intent1 = new Intent(NVDActivity.this, RegisteredTravelsFragment.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.nav_gallery:
+                    Intent intent2 = new Intent(NVDActivity.this, CompanyTravelsFragment.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.nav_slideshow:
+                    Intent intent3 = new Intent(NVDActivity.this, HistoryTravelsFragment.class);
+                    startActivity(intent3);
+                    break;
+            }
+            return true;
+        });
+*/
 
         addInitialData();
     }
@@ -84,48 +111,62 @@ public class NVDActivity extends AppCompatActivity {
      * Add some initial data in order to test if the application is working
      * temporary method for debugging
      */
-    private void addInitialData(){
+    private void addInitialData() {
         try {
 
             Travel t1 = new Travel();
             t1.setClientName("Avi Cohen");
             t1.setClientEmail("avi@gmail.com");
             t1.setClientPhone("021234567");
-            t1.setPickupAddress(new UserLocation(1,2));
+            t1.setPickupAddress(new UserLocation(1, 2));
             t1.setNumOfPassengers(2);
-            t1.travelDateTypeSetter(new Date(2020,2,2));
+            t1.travelDateTypeSetter(new Date(2020, 2, 2));
             t1.setChildrenTransportation(true);
             t1.setSafeGuarded(true);
-            t1.setDestAddress(new UserLocation(3,2));
+            t1.setDestAddress(new UserLocation(3, 2));
             viewModel.addTravel(t1);
 
             Travel t2 = new Travel();
             t2.setClientName("Hedva Izhaki");
             t2.setClientEmail("hedva@gmail.com");
             t2.setClientPhone("021224597");
-            t2.setPickupAddress(new UserLocation(3,4));
+            t2.setPickupAddress(new UserLocation(3, 4));
             t2.setNumOfPassengers(2);
-            t2.travelDateTypeSetter(new Date(2021,1,2));
+            t2.travelDateTypeSetter(new Date(2021, 1, 2));
             t2.setChildrenTransportation(true);
             t2.setSafeGuarded(true);
-            t2.setDestAddress(new UserLocation(7,2));
+            t2.setDestAddress(new UserLocation(7, 2));
             viewModel.addTravel(t2);
 
             Travel t3 = new Travel();
             t3.setClientName("David Levi");
             t3.setClientEmail("david@gmail.com");
             t3.setClientPhone("084224596");
-            t3.setPickupAddress(new UserLocation(9,9));
+            t3.setPickupAddress(new UserLocation(9, 9));
             t3.setNumOfPassengers(4);
-            t3.travelDateTypeSetter(new Date(2020,9,2));
+            t3.travelDateTypeSetter(new Date(2020, 9, 2));
             t3.setChildrenTransportation(false);
             t3.setSafeGuarded(true);
-            t3.setDestAddress(new UserLocation(1,8));
+            t3.setDestAddress(new UserLocation(1, 8));
             viewModel.addTravel(t3);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_sign_out:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(NVDActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
