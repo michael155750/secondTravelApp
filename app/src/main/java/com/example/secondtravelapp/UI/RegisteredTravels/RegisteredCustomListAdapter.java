0aecb76411d510e7786433eb7ddc0758dc97beba
asaf0 <hillelassafd@gmail.com>
2021-01-13 14:27:54 +0200

    Adapter not working

--- app/src/main/java/com/example/secondtravelapp/UI/RegisteredTravels/RegisteredCustomListAdapter.java
+++ app/src/main/java/com/example/secondtravelapp/UI/RegisteredTravels/RegisteredCustomListAdapter.java
@@ -4,24 +4,35 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 import com.example.secondtravelapp.Models.Travel;
 import com.example.secondtravelapp.R;
-import com.example.secondtravelapp.UI.HistoryTravels.HistoryCustomListAdapter;
 
 
 public class RegisteredCustomListAdapter extends BaseAdapter{
     private Context context;
+    private ArrayList<String> companySpinner;
+    private ArrayList<String> statusSpinner;
     private ArrayList<Travel> travels;
 
-    public RegisteredCustomListAdapter(Context context, ArrayList<Travel> travels) {
+
+    public RegisteredCustomListAdapter(/*Context context, ArrayList<Travel> travels,
+                                       ArrayList<String> spinnerItems, ArrayList<String> type*/
+            ArrayList<Travel> travels, ArrayList<String> spinnerItems,
+            ArrayList<String> type,
+            Context context) {
         this.context = context;
         this.travels = travels;
+        this.companySpinner = spinnerItems;
+        this.statusSpinner = type;
     }
 
     @Override
@@ -41,28 +52,70 @@ public class RegisteredCustomListAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        RegisteredCustomListAdapter.ViewHolder viewHolder;
+        /*RegisteredCustomListAdapter.ViewHolder viewHolder;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.history_adapter_row, parent, false);
-            viewHolder = new RegisteredCustomListAdapter.ViewHolder(convertView);
-            convertView.setTag(viewHolder);
+            //viewHolder = new RegisteredCustomListAdapter.ViewHolder(convertView);
+            //convertView.setTag(viewHolder);
         } else {
             viewHolder = (RegisteredCustomListAdapter.ViewHolder) convertView.getTag();
         }
-
         Travel currentItem = (Travel) getItem(position);
-        //viewHolder.source.setText();
-        //viewHolder.dest.setText();
-        viewHolder.date.setText(currentItem.getTravelDate());
+         */
 
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.registered_adapter_row, null);
+        }
 
+        TextView textView = (TextView) view.findViewById(R.id.source_registered);//source
+        TextView date = view.findViewById(R.id.date_registered);
+        TextView destination = view.findViewById(R.id.dest_registered);
 
+        Spinner companySpinner = (Spinner) view.findViewById(R.id.compay_registered);
+        Spinner type = view.findViewById(R.id.status_registered);
 
+        //textView.setText(mData.get(position).getSource());
+        date.setText(travels.get(position).getTravelDate());
+        //destination.setText(mData.get(position).getDest());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, this.companySpinner);
+        companySpinner.setAdapter(adapter);
+        companySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code he
+                // Toast.makeText(context, companySpinner.get(position), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here//Select
+            }
+
+        });
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, statusSpinner);
+        type.setAdapter(adapter1);
+        type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                Toast.makeText(context, statusSpinner.get(position), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
 
         return convertView;
     }
 
+    /*
     //ViewHolder inner class
     private class ViewHolder {
 
@@ -77,10 +130,13 @@ public class RegisteredCustomListAdapter extends BaseAdapter{
 
         public ViewHolder(View view) {
 
-            source = (TextView)view.findViewById(R.id.source_registered);
-            dest = (TextView)view.findViewById(R.id.dest_registered);
+            //source = (TextView)view.findViewById(R.id.source_registered);
+            //dest = (TextView)view.findViewById(R.id.dest_registered);
             date = (TextView)view.findViewById(R.id.date_registered);
+            status = view.findViewsWithText(R.id.status_registered);
 
         }
-    }
+
+     */
+
 }
--- app/src/main/java/com/example/secondtravelapp/UI/RegisteredTravels/RegisteredTravelsFragment.java
+++ app/src/main/java/com/example/secondtravelapp/UI/RegisteredTravels/RegisteredTravelsFragment.java
@@ -1,26 +1,33 @@
 package com.example.secondtravelapp.UI.RegisteredTravels;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.secondtravelapp.Models.Travel;
 import com.example.secondtravelapp.R;
 import com.example.secondtravelapp.UI.MainTravelsViewModel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * class registered travels fragment represents
 
  */
 public class RegisteredTravelsFragment extends Fragment {
 
-    private MainTravelsViewModel galleryViewModel;
+    private MainTravelsViewModel viewModel;
+    public RegisteredCustomListAdapter adapter;
+    private ListView listView;
+
 
     /**
      *
@@ -31,9 +38,74 @@ public class RegisteredTravelsFragment extends Fragment {
      */
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
+        viewModel =
                 new ViewModelProvider(this).get(MainTravelsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_registered_travels, container, false);
+        listView = (ListView) root.findViewById(R.id.listview_registered);
+
+
+        //
+        ArrayList<Travel> mData = new ArrayList<>();
+        Travel t1 = new Travel();
+        t1.setClientName("Avi Cohen");
+        t1.setClientEmail("avi@gmail.com");
+        t1.setClientPhone("021234567");
+        //t1.setPickupAddress(new UserLocation(1, 2));
+        t1.setNumOfPassengers(2);
+        t1.travelDateTypeSetter(new Date(2020, 2, 2));
+        t1.setChildrenTransportation(true);
+        t1.setSafeGuarded(true);
+        //t1.setSource("Jerusalem, Hacotel");
+        //t1.setDest("Ramat Gan");
+        //t1.setDestAddress(new UserLocation(3, 2));
+        t1.setStatus(Travel.RequestType.paid);
+        mData.add(t1);
+
+        Travel t2 = new Travel();
+        t2.setClientName("Hedva Izhaki");
+        t2.setClientEmail("hedva@gmail.com");
+        t2.setClientPhone("021224597");
+        //t2.setPickupAddress(new UserLocation(3, 4));
+        t2.setNumOfPassengers(2);
+        t2.travelDateTypeSetter(new Date(2021, 1, 2));
+        t2.setChildrenTransportation(true);
+        t2.setSafeGuarded(true);
+        //t2.setSource("Tel Aviv");
+        //t2.setDest("Machon Lev");
+        //t2.setDestAddress(new UserLocation(7, 2));
+        t2.setStatus(Travel.RequestType.close);
+        mData.add(t2);
+
+        Travel t3 = new Travel();
+        t3.setClientName("David Levi");
+        t3.setClientEmail("david@gmail.com");
+        t3.setClientPhone("084224596");
+        //t3.setPickupAddress(new UserLocation(9, 9));
+        t3.setNumOfPassengers(4);
+        t3.travelDateTypeSetter(new Date(2020, 9, 2));
+        t3.setChildrenTransportation(false);
+        t3.setSafeGuarded(true);
+        //t3.setSource("Jerusalem, Hacotel");
+        //t3.setDest("Machon Lev");
+        //t3.setDestAddress(new UserLocation(1, 8));
+        mData.add(t3);
+
+        ArrayList<String> mSpinnerData = new ArrayList<>();
+        //mSpinnerData.add(0, "Chose company");//Select
+        mSpinnerData.add("Dan");
+        mSpinnerData.add("Egged");
+        mSpinnerData.add("Metropolin");
+        mSpinnerData.add("Kavim");
+
+        ArrayList<String> typeSpinnerData = new ArrayList<>();
+        //typeSpinnerData.add(0, "Select");//Accepted
+        typeSpinnerData.add("Accepted");
+        typeSpinnerData.add("Run");
+        typeSpinnerData.add("Close");
+        typeSpinnerData.add("Paid");
+
+
+        adapter = new RegisteredCustomListAdapter(mData, mSpinnerData, typeSpinnerData, getActivity());
         return root;
     }
 }
